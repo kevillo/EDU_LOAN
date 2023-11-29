f22cc6912f61c3d5d1aececde2376adb56d71824
leticiadz <diazleticia228@gmail.com>
2023-11-29 17:51:21 -0600

    logo

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LogController extends Controller
 {
 
     public function store(Request $request)
     {
-
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
         $request->validate([
             'id_usuario' => 'required',
             'tabla_afectada' => 'required',
--- app/Http/Controllers/TipoEquipoController.php
+++ app/Http/Controllers/TipoEquipoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use App\Models\TipoEquipo;
 use Illuminate\Http\Request;
 
@@ -10,12 +11,18 @@ class TipoEquipoController extends Controller
     // function create
     public function create()
     {
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
         return view('tipo_equipos.create');
     }
 
     // function store
     public function store(Request $request)
     {
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
         $request->validate([
             'descripcion_tipo_equipo' => 'required',
         ]);
